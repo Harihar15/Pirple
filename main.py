0cd9a31282a8d211c9eadc61fcd043ef507df8d5
Harihar15 <52389316+Harihar15@users.noreply.github.com>
2019-07-09 15:22:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+''' Favourite Song Details '''
+
+Artist = "Dj Snake"
+Ft_Artist = "Justin Bieber"
+Song = "Let Me Love You"
+Genre = "Dance/Electronic"
+duration_period = 205                                                  # in seconds
+release_year = 2016
+Album = "Encore"
+VideoLink = "https://www.youtube.com/watch?v=euCqAq6BRa4"              # youtube link
+Views_on_youtube = "741Million"                                        # as per july 2019
+Available_platform = "Spotify, Amazon Prime, Youtube Music,"
+
+print("Artist:", Artist)
+
+print("--------")
+
+print("Ft_Artist:", Ft_Artist)
+
+print("--------")
+
+print("Song:", Song)
+
+print("--------")
+
+print("Genre:", Genre)
+
+print("--------")
+
+print("Duration Period:", duration_period)
+
+print("--------")
+
+print("Release Year:", release_year)
+
+print("--------")
+
+print("Album:", Album)
+
+print("--------")
+
+print("Youtube Link for song", VideoLink)
+
+print("--------")
+
+print("Views on youtube:", Views_on_youtube)
+
+print("--------")
+
+print("Available on:", Available_platform, "ETC.")
+
