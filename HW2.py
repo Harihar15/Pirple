9cf09d4f0c573ce554dd3b1d067b71d5bcc36835
Harihar15 <52389316+Harihar15@users.noreply.github.com>
2019-07-10 15:47:58 +0530

    HW2

---
+++ HW2.py
@@ -0,0 +1,27 @@
+# Song's Genre
+def genre():
+
+    print("Dance/Electronic")
+
+
+genre()
+
+# Song's Artist
+def artist():
+
+    print("Dj Snake")
+
+
+artist()
+
+# Song's Year
+def year():
+
+    print(2016)
+
+
+year()
+
+
+
+
